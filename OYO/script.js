1dabe3d593876af18a54ad16a7fbbf09525456ce
Alpesh Patil <a14sh5@gmail.com>
2017-12-09 16:36:02 +0530

    Update script.js

--- OYO/script.js
+++ OYO/script.js
@@ -28,7 +28,7 @@ function bakeDataCell(acc){
 
 function fetchData(resJson){
 
-	data = resJson;
+	data = JSON.parse(resJson);
 	console.log("Recv "+resJson.length);
 
 	var val = document.getElementById("rName").value.toLowerCase();
