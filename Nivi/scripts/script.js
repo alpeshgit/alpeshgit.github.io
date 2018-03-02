b803bb56cc0b52ba59011edcd23be0e68188b99b
Alpesh Patil <a14sh5@gmail.com>
2018-03-02 13:11:20 +0530

    Update script.js

--- Nivi/scripts/script.js
+++ Nivi/scripts/script.js
@@ -209,14 +209,22 @@ myApp.controller("appController",function($scope, $http){
                         geo : {
                                 latitude: position.coords.latitude,
                                 longitude: position.coords.longitude
+                            },
+                        encode : function(this) {
+                            var p = [];
+                             for (var key in this) {
+                                p.push(key + '=' + encodeURIComponent(this[key]));
                             }
+                            return p.join('&');
+                        }
         };	
-                
+            
+        
             $http({
                 method: 'POST',
                 url: "https://jurenu-developer-edition.ap5.force.com/sana/services/apexrest/eateries",
-                data: JSON.stringify(param),
-                headers: {'Content-Type': 'application/form-data'}
+                data: param.encode(),
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
             })
             .then(function(response) {
                 var resp = response.data;
